18d9251afcbef74abc36dd14b68f4d46a54215dd
Rahul9205 <rahul.931201@gmail.com>
2024-03-18 17:52:56 +0530

    nextjs -website

---
+++ constants/index.tsx
@@ -0,0 +1,137 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Stripe Payment",
+    Image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    title: "Auto Voyage",
+    text: "Your Journey, Your Way. A premier car rental service offering a wide range of vehicles for your every need, ensuring a comfortable and memorable journey.",
+    src: "/car_rentel.png",
+  },
+  {
+    title: "PassFort",
+    text: "Your Fortress of Security. An innovative tool that generates robust and secure passwords. It’s your key to safeguarding your digital life.",
+    src: "/Password-Generator.png",
+  },
+  {
+    title: "Modern Nextjs Portfolio",
+    text: "A modern Next.js portfolio is a dynamic, fast, and SEO-friendly showcase of one’s skills and projects. It leverages the power of React and server-side rendering, offering an interactive user experience.",
+    src: "/WebPortfolio.png",
+  },
+  {
+    title: "comming soon...",
+    text: "✌️✌️✌️✌️✌️✌️✌️✌️✌️✌️✌️✌️",
+    src: "/coming-soon.webp",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
